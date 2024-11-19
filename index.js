07c924a7ecc82b70915e3352ed6c0475c60defda
VND ut <2251120143@ut.edu.vn>
2024-11-19 17:57:08 +0700

    update

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 const fetch = require("node-fetch");
 const fs = require("fs");
 
-// URL API lấy giá Bitcoin
 const apiUrl =
   "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd";
 
 fetch(apiUrl)
   .then((response) => response.json())
   .then((data) => {
-    // Lấy giá Bitcoin
-    const btc = data["bitcoin"]["usd"];
-    console.log(`Current Bitcoin Price: $${btc}`);
+    const btcPrice = data.bitcoin.usd;
+    console.log(`Bitcoin price fetched: $${btcPrice}`);
 
-    // Đọc nội dung từ file template.md
-    let template = fs.readFileSync("template.md", "utf8");
+    let readme = fs.readFileSync("README.md", "utf8");
+    readme = readme.replace(
+      /Bitcoin Price: \$\d+(\.\d+)?/,
+      `Bitcoin Price: $${btcPrice} USD`
+    );
+    fs.writeFileSync("README.md", readme);
 
-    // Thay thế placeholder {BITCOIN} bằng giá Bitcoin
-    const updatedContent = template.replace("{BITCOIN}", btc);
-
-    // Ghi lại nội dung đã cập nhật vào README.md
-    fs.writeFileSync("README.md", updatedContent);
-
-    console.log("README.md has been updated with the latest Bitcoin price.");
+    console.log("README.md has been updated.");
   })
   .catch((error) => {
     console.error("Error fetching Bitcoin price:", error);
+    process.exit(1);
   });
